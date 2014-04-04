fbfa1a9f55d5b9501b2dac7aa20c2d59c2526add
luuxx <luuky19@gmail.com>
2014-04-04 15:27:24 +0200

    Why Windows Why

--- workspace/GLES_test/src/dg/maincode/gles_test/HomeActivity.java
+++ workspace/GLES_test/src/dg/maincode/gles_test/HomeActivity.java
@@ -3,19 +3,27 @@ package dg.maincode.gles_test;
 import android.app.Activity;
 import android.app.DownloadManager;
 import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
+	DownLoadComplte mDownload;
+	DownloadManager downloadmanager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_activity_layout);
+		mDownload = new DownLoadComplte();
 		Button startGLbutton = (Button) findViewById(R.id.ButtonStartGL);
 		startGLbutton.setOnClickListener(new OnClickListener() {
 			
@@ -23,16 +31,43 @@ public class HomeActivity extends Activity {
 			public void onClick(View v) {
 				//download stuff
 				String servicestring = Context.DOWNLOAD_SERVICE;
-			    DownloadManager downloadmanager;
+				EditText urlbar = (EditText) findViewById(R.id.stl_url_bar);
 			    downloadmanager = (DownloadManager) getSystemService(servicestring);
-			    Uri uri = Uri.parse("https://www.google.nl/images/srpr/logo11w.png");
+			    Uri uri = Uri.parse(urlbar.getText().toString());
 			    DownloadManager.Request request = new Request(uri);
 			    Long reference = downloadmanager.enqueue(request);
-				Intent startnext = new Intent(HomeActivity.this, MainActivity.class);
-				startActivity(startnext);
+			    mDownload = new DownLoadComplte();
+		        registerReceiver(mDownload, new IntentFilter(
+		        DownloadManager.ACTION_DOWNLOAD_COMPLETE));
 			}
 		});
 		
 	}
-	
+	private class DownLoadComplte extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+        	String filename = "";
+            if (intent.getAction().equalsIgnoreCase(
+                    DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
+                Toast.makeText(context, "Download Complete", Toast.LENGTH_LONG)
+                        .show();
+                Bundle extras = intent.getExtras();
+                DownloadManager.Query q = new DownloadManager.Query();
+                q.setFilterById(extras.getLong(DownloadManager.EXTRA_DOWNLOAD_ID));
+                Cursor c = downloadmanager.query(q);
+                if (c.moveToFirst()) {
+                    int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                    if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                        // process download
+                        filename = c.getString(c.getColumnIndex(DownloadManager.COLUMN_TITLE));
+                        // get other required data by changing the constant passed to getColumnIndex
+                    }
+                }
+                Intent startnext = new Intent(HomeActivity.this, MainActivity.class);
+                intent.putExtra("STLfile", filename);
+				startActivity(startnext);
+            }
+        }
+    }
 }
